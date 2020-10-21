19e399e0cba3f5ccccefd8070737cd2a30f9771c
jstefanska <44728213+jstefanska@users.noreply.github.com>
2020-10-21 17:28:37 +0200

    Add redis version

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 import base64
-import requests
-import psycopg2
 import os
-from datetime import datetime
+import redis
+import requests
+import json
 
 # generate bearer token from api_key and api_secret (provided in Twitter API project)
 # api_key and api_secret should be set as environment variable for security
@@ -44,42 +44,51 @@ else:
     print("Bearer token authorization failed. Check your API key and API secret.")
     exit(1)
 
-# request 10 recent Tweets
 
-if 'DB_PASS' not in os.environ:
-    print("'DB_PASS' environment variable does not exist")
-    exit(1)
-elif 'DB_HOST' not in os.environ:
-    print("'DB_HOST' environment variable does not exist")
-    exit(1)
-elif 'DB_NAME' not in os.environ:
-    print("'DB_NAME' environment variable does not exist")
-    exit(1)
-elif 'DB_USER' not in os.environ:
-    print("'DB_USER' environment variable does not exist")
-    exit(1)
-elif 'DB_PORT' not in os.environ:
-    print("'DB_PORT' environment variable does not exist")
-    exit(1)
-else:
-    db_pass = os.getenv('DB_PASS')
-    db_host = os.getenv('DB_HOST')
-    db_name = os.getenv('DB_NAME')
-    db_user = os.getenv('DB_USER')
-    db_port = os.getenv('DB_PORT')
+# request 10 recent Tweets
+#
+# if 'DB_PASS' not in os.environ:
+#     print("'DB_PASS' environment variable does not exist")
+#     exit(1)
+# elif 'DB_HOST' not in os.environ:
+#     print("'DB_HOST' environment variable does not exist")
+#     exit(1)
+# elif 'DB_NAME' not in os.environ:
+#     print("'DB_NAME' environment variable does not exist")
+#     exit(1)
+# elif 'DB_USER' not in os.environ:
+#     print("'DB_USER' environment variable does not exist")
+#     exit(1)
+# elif 'DB_PORT' not in os.environ:
+#     print("'DB_PORT' environment variable does not exist")
+#     exit(1)
+# else:
+#     db_pass = os.getenv('DB_PASS')
+#     db_host = os.getenv('DB_HOST')
+#     db_name = os.getenv('DB_NAME')
+#     db_user = os.getenv('DB_USER')
+#     db_port = os.getenv('DB_PORT')
+
+class Tweet:
+    tweet_content = ""
+    tweet_id = ""
+
+    def __init__(self, tweet_id, tweet_content):
+        self.tweet_id = tweet_id
+        self.tweet_content = tweet_content
+
 
 try:
 
-    # select posted hashtags from hashtags table in DB
-    conn = psycopg2.connect(host=db_host, port=db_port, dbname=db_name, user=db_user,
-                            password=db_pass)
-    conn.autocommit = True
-    cur = conn.cursor()
+    r = redis.StrictRedis(
+        host='localhost',
+        port=6379,
+        password='mysecretpassword',
+        charset="utf-8",
+        decode_responses=True)
 
-    cur.execute("select hashtag from hashtags")
-    hashtag = cur.fetchall()
+    hashtag = r.smembers('hashtags')
 
-    # request Tweets with each selected hashtag
     for x in hashtag:
         search_headers = {
             'Authorization': 'Bearer {}'.format(access_token)
@@ -97,23 +106,15 @@ try:
 
         tweet_data = search_resp.json()
 
-        datetime = datetime.now()
+        for status in tweet_data['statuses']:
+            tweet = Tweet(status['id_str'], status['text'])
+            tweet_json = str(json.dumps(tweet.__dict__))
+            r.lpush(x, tweet_json)
 
-        # insert requested Tweets to DB
+    r.close()
+    print('DB updated with new tweets.')
+    exit(0)
 
-        twitter_row = [(status['text'], status['id'], x, datetime) for status in tweet_data['statuses']]
-
-        cur.executemany(
-            "INSERT INTO public.twitter (tweet_content, tweet_id, hashtag, datetime) VALUES (%s,%s,%s,%s)",
-            twitter_row)
-
-    cur.close()
-    conn.close()
-
-except psycopg2.OperationalError as e:
-    print("Unable to connect to the database!".format(e))
-    exit(1)
-
-print("Rows are updated.")
-exit(0)
-
+except Exception as ex:
+    print('Error:', ex)
+    exit('Failed to connect, terminating.')
